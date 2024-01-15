21097c0d7ac08fadf5ac3b4680ea8ce0224c89f4
lukasz <zed11temp@gmail.com>
2024-01-15 14:14:26 +0100

    filter by categories done✅ && small modif of pagination buttons in order for the categories filters to work

--- frontend/src/components/buttons/ButtonsPagination.tsx
+++ frontend/src/components/buttons/ButtonsPagination.tsx
@@ -14,6 +14,11 @@ function ButtonsPagination({ count, page, limit }: ButtonsPaginationProps) {
   const maxPages = Math.ceil(count / Number(limit));
   const activePage = Number(router.query.page) || 1;
 
+  function onClickPageNumber(index: number) {
+    const queryString = router.asPath.replace(/(page=)\d+/, `$1${index}`);
+    router.push(`${queryString}`);
+  }
+
   const buttons = [];
   const beginPagination =
     activePage > 3 ? activePage - 3 : activePage - (activePage - 1);
@@ -22,58 +27,74 @@ function ButtonsPagination({ count, page, limit }: ButtonsPaginationProps) {
 
   for (let i = beginPagination; i <= endPagination; i++) {
     buttons.push(
-      <Link
+      <div
         key={i}
-        href={`/?page=${i}&limit=${limit}`}
         className={`${buttonsPaginationStyles["link-page"]} ${
           i === Number(page) ? buttonsPaginationStyles["link-page-active"] : ""
         }`}
-        onClick={() => {
-          router.push(`/?page=${i}&limit=${limit}`);
-        }}
+        onClick={() => onClickPageNumber(i)}
       >
         {i}
-      </Link>
+      </div>
     );
   }
 
   return (
     <div className={buttonsPaginationStyles["pages-links-container"]}>
       <button
-        className={`${buttonsPaginationStyles["arrow"]} ${buttonsPaginationStyles["two-arrow-left"]} ${activePage === 1 && buttonsPaginationStyles["hide"]} `}
+        className={`${buttonsPaginationStyles["arrow"]} ${
+          buttonsPaginationStyles["two-arrow-left"]
+        } ${activePage === 1 && buttonsPaginationStyles["hide"]} `}
+        // takes query url example: /?page=10&limit=20&categories=[] and replace page number on 1
         onClick={() => {
-          router.push(`/?page=1&limit=${limit}`);
+          const queryString = router.asPath.replace(/(page=)\d+/, `$1${1}`);
+          router.push(`${queryString}`);
         }}
       >
         {"<<"}
       </button>
       <button
-        className={`${buttonsPaginationStyles["arrow"]} ${buttonsPaginationStyles["one-arrow-left"]} ${activePage === 1 && buttonsPaginationStyles["hide"]} `}
+        className={`${buttonsPaginationStyles["arrow"]} ${
+          buttonsPaginationStyles["one-arrow-left"]
+        } ${activePage === 1 && buttonsPaginationStyles["hide"]} `}
         onClick={() => {
-          router.push(
-            `/?page=${Number(page) > 1 ? Number(page) - 1 : "1"}&limit=${limit}`
+          // takes query url example: /?page=1&limit=20&categories=[] and replace page number on --1
+          const queryString = router.asPath.replace(
+            /(page=)\d+/,
+            `$1${Number(page) > 1 ? Number(page) - 1 : "1"}`
           );
+          router.push(`${queryString}`);
         }}
       >
         {"<"}
       </button>
       {buttons.length > 0 && buttons.map((button) => button)}
       <button
-        className={`${buttonsPaginationStyles["arrow"]} ${buttonsPaginationStyles["one-arrow-right"]} ${activePage === maxPages && buttonsPaginationStyles["hide"]} `}
+        className={`${buttonsPaginationStyles["arrow"]} ${
+          buttonsPaginationStyles["one-arrow-right"]
+        } ${activePage === maxPages && buttonsPaginationStyles["hide"]} `}
         onClick={() => {
-          router.push(
-            `/?page=${
-              Number(page) < maxPages ? Number(page) + 1 : maxPages
-            }&limit=${limit}`
+          // takes query url example: /?page=1&limit=20&categories=[] and replace page number on ++1
+          const queryString = router.asPath.replace(
+            /(page=)\d+/,
+            `$1${Number(page) < maxPages ? Number(page) + 1 : maxPages}`
           );
+          router.push(`${queryString}`);
         }}
       >
         {">"}
       </button>
       <button
-        className={`${buttonsPaginationStyles["arrow"]} ${buttonsPaginationStyles["two-arrow-right"]} ${activePage === maxPages && buttonsPaginationStyles["hide"]} `}
+        className={`${buttonsPaginationStyles["arrow"]} ${
+          buttonsPaginationStyles["two-arrow-right"]
+        } ${activePage === maxPages && buttonsPaginationStyles["hide"]} `}
         onClick={() => {
-          router.push(`/?page=${maxPages}&limit=${limit}`);
+          // takes query url example: /?page=1&limit=20&categories=[] and replace page number on maxPages
+          const queryString = router.asPath.replace(
+            /(page=)\d+/,
+            `$1${maxPages}`
+          );
+          router.push(`${queryString}`);
         }}
       >
         {">>"}
